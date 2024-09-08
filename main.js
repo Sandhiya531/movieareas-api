8fea05742cadd99f4ce4440b4b18c3dd40e5f91f
Sandhiya <sandhiya.g@ei.study>
2024-09-09 00:51:02 +0530

    route structure

--- main.js
+++ main.js
@@ -2,9 +2,30 @@ const express=require('express')
 const app=express()
 const port=3000
 
-
 app.get('/',(req,res)=>{
     res.json({msg: "Success"})
+})
+
+//CRUD functionality of movies
+
+//R - For Reading/Fetching moview
+app.get('/movies',()=>{
+
+})
+
+//C- For Creating movies
+app.post('/movies',()=>{
+
+})
+
+//U-For Update movie
+app.put('/movies/:id',()=>{
+
+})
+
+//D-For delete moview
+app.delete('/movies/:id',()=>{
+    
 })
 
 app.listen(port,()=>{
